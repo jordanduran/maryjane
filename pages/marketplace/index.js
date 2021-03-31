b3c4a1ab182188b5c89efa4a4dcec8d0a1d4ca49
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2021-03-31 19:07:56 -0400

    Max-width on marketplace page container

--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -5,7 +5,7 @@ const MarketPlacePage = () => {
   return (
     <div className='max-w-screen-xl mx-auto px-4 sm:px-4 lg:px-8 confetti-bg'>
       <Hero />
-      <div className='max-w-5xl mx-auto'>
+      <div className='max-w-5xl md:max-w-4xl lg:max-w-7xl mx-auto'>
         <CompanyList />
       </div>
     </div>
