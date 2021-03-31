0b3d8feeeac5b8d0e05caddb6abc6d89636003b7
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2021-03-31 15:50:55 -0400

    NextAuth provider for session

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
+import { Provider } from 'next-auth/client';
 import Layout from '../components/layout/Layout';
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider session={pageProps.session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 };
 
