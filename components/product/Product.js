8fd306b201e3916c8c03d184fa702806a532ca95
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2021-04-06 14:58:41 -0400

    Dynamic page routing for products

---
+++ components/product/Product.js
@@ -0,0 +1,9 @@
+const Product = (props) => {
+  return (
+    <div>
+      <h1>Product</h1>
+    </div>
+  );
+};
+
+export default Product;
--- pages/marketplace/[companyId]/[productId]/index.js
+++ pages/marketplace/[companyId]/[productId]/index.js
@@ -1,15 +1,51 @@
 import { useRouter } from 'next/router';
+import Product from '../../../../components/product/Product';
 
 const ProductPage = () => {
   const router = useRouter();
 
-  // console.log(router.query.productId);
-
+  // const companyId = router.query.companyId;
+  
   return (
     <div>
-      <h1>Product Page</h1>
+      <Product id='p1' />
     </div>
   );
 };
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          productId: '/p1',
+        },
+        params: {
+          productId: 'p2',
+        },
+        params: {
+          productId: 'p3',
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const productId = context.params.productId;
+  const companyId = context.params.companyId;
+
+  // Fetch data for single product
+
+  return {
+    props: {
+      productData: {
+        companyId: companyId,
+        id: productId,
+      },
+    },
+  };
+}
 
 export default ProductPage;
