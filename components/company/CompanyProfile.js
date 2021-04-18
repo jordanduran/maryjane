4a2b9d31fb5a78c7583ccce60c6cf0a5c2d44352
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2021-04-18 15:05:58 -0400

    Verified badge UI on company profile

--- components/company/CompanyProfile.js
+++ components/company/CompanyProfile.js
@@ -28,23 +28,40 @@ const CompanyProfile = (props) => {
                     />
                   </div>
                   <div className='ml-3'>
-                    <p className='text-sm font-bold text-gray-700 group-hover:text-gray-900'>
-                      <svg
-                        className='h-5 inline-block mr-1'
-                        xmlns='http://www.w3.org/2000/svg'
-                        fill='none'
-                        viewBox='0 0 24 24'
-                        stroke='currentColor'
-                      >
-                        <path
-                          strokeLinecap='round'
-                          strokeLinejoin='round'
-                          strokeWidth='2'
-                          d='M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207'
-                        />
-                      </svg>
-                      {props.company}
-                    </p>
+                    <div className='flex justify-between items-center'>
+                      <p className='flex text-sm font-bold text-gray-700 group-hover:text-gray-900'>
+                        <svg
+                          className='h-5 inline-block mr-1'
+                          xmlns='http://www.w3.org/2000/svg'
+                          class='h-5 w-5'
+                          viewBox='0 0 20 20'
+                          fill='currentColor'
+                        >
+                          <path
+                            fill-rule='evenodd'
+                            d='M4 4a2 2 0 012-2h8a2 2 0 012 2v12a1 1 0 110 2h-3a1 1 0 01-1-1v-2a1 1 0 00-1-1H9a1 1 0 00-1 1v2a1 1 0 01-1 1H4a1 1 0 110-2V4zm3 1h2v2H7V5zm2 4H7v2h2V9zm2-4h2v2h-2V5zm2 4h-2v2h2V9z'
+                            clip-rule='evenodd'
+                          />
+                        </svg>
+
+                        <span className='text-sm'>{props.company}</span>
+                      </p>
+                      <p className='text-sm font-bold text-green-500'>
+                        <svg
+                          className='h-6 inline-block mr-1'
+                          xmlns='http://www.w3.org/2000/svg'
+                          class='h-5 w-5'
+                          viewBox='0 0 20 20'
+                          fill='currentColor'
+                        >
+                          <path
+                            fill-rule='evenodd'
+                            d='M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z'
+                            clip-rule='evenodd'
+                          />
+                        </svg>
+                      </p>
+                    </div>
                     <p className='text-sm'>
                       <svg
                         className='h-5 mr-1 inline-block'
