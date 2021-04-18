66b19cf5c196a879ec8d9eac6fee61abdf110bdc
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2021-04-18 15:25:46 -0400

    Outline heroicon

--- components/company/CompanyProfile.js
+++ components/company/CompanyProfile.js
@@ -31,16 +31,17 @@ const CompanyProfile = (props) => {
                     <div className='flex justify-between items-center'>
                       <p className='flex text-sm font-bold text-gray-700 group-hover:text-gray-900'>
                         <svg
-                          className='h-5 inline-block mr-1'
+                          className='h-5 w-5 inline-block mr-1'
                           xmlns='http://www.w3.org/2000/svg'
-                          class='h-5 w-5'
-                          viewBox='0 0 20 20'
-                          fill='currentColor'
+                          fill='none'
+                          viewBox='0 0 24 24'
+                          stroke='currentColor'
                         >
                           <path
-                            fill-rule='evenodd'
-                            d='M4 4a2 2 0 012-2h8a2 2 0 012 2v12a1 1 0 110 2h-3a1 1 0 01-1-1v-2a1 1 0 00-1-1H9a1 1 0 00-1 1v2a1 1 0 01-1 1H4a1 1 0 110-2V4zm3 1h2v2H7V5zm2 4H7v2h2V9zm2-4h2v2h-2V5zm2 4h-2v2h2V9z'
-                            clip-rule='evenodd'
+                            strokeLinecap='round'
+                            strokeLinejoin='round'
+                            strokeWidth='2'
+                            d='M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4'
                           />
                         </svg>
 
@@ -48,16 +49,15 @@ const CompanyProfile = (props) => {
                       </p>
                       <p className='text-sm font-bold text-green-500'>
                         <svg
-                          className='h-6 inline-block mr-1'
+                          className='h-6 w-6 inline-block mr-1'
                           xmlns='http://www.w3.org/2000/svg'
-                          class='h-5 w-5'
                           viewBox='0 0 20 20'
                           fill='currentColor'
                         >
                           <path
-                            fill-rule='evenodd'
+                            fillRule='evenodd'
                             d='M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z'
-                            clip-rule='evenodd'
+                            clipRule='evenodd'
                           />
                         </svg>
                       </p>
