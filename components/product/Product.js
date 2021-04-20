f6358c8c9604c5ebec3c2bba9d578e48a24f29f6
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2021-04-20 15:06:48 -0400

    Removed console log from Product component

--- components/product/Product.js
+++ components/product/Product.js
@@ -141,7 +141,7 @@ const Product = (props) => {
     }
     setQtyOfProduct(0);
   };
-  console.log(props);
+
   return (
     <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 m-3'>
       <div className='max-w-3xl mx-auto'>
